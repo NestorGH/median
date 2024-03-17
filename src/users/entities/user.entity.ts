f1ca16b84f7e72432112d444f78bdee9f967c1b3
NestorGH <nestornica10@gmail.com>
2024-03-17 16:31:06 -0600

    updated user entity and dto

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { ApiProperty } from '@nestjs/swagger';
+import { User } from '@prisma/client';
+
+export class UserEntity implements User {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  email: string;
+
+  //Avoid set var like password into swagger with @ApiProperty
+  password: string;
+}
