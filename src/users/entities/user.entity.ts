d9b66336250b312b251baf93db543f3f056b0035
NestorGH <nestornica10@gmail.com>
2024-03-17 21:13:27 -0600

    Added serializer interceptor

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { User } from '@prisma/client';
+import { Exclude } from 'class-transformer';
 
 export class UserEntity implements User {
+  //copy any subset of the properties defined in the UserEntity class
+  constructor(partial: Partial<UserEntity>) {
+    Object.assign(this, partial);
+  }
+
   @ApiProperty()
   id: number;
 
@@ -18,5 +24,6 @@ export class UserEntity implements User {
   email: string;
 
   //Avoid set var like password into swagger with @ApiProperty
+  @Exclude()
   password: string;
 }
