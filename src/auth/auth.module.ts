9be149d830a272cec918a6ddbcba198e069b582e
NestorGH <nestornica10@gmail.com>
2024-03-23 19:45:02 -0600

    added JwtStrategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,8 @@ import { AuthController } from './auth.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from 'src/users/users.module';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 export const jwtSecret = process.env.JWT_SECRET;
 
@@ -15,8 +17,9 @@ export const jwtSecret = process.env.JWT_SECRET;
       secret: jwtSecret,
       signOptions: { expiresIn: '5m' }, // e.g. 30s, 7d, 24h
     }),
+    UsersModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
